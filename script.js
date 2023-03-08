e1950a91fc023c63221fbf5e19613ea093ba06d7
erikchiodo <erikchiodo@gmail.com>
2023-03-07 22:12:52 -0500

    Finalized Javascript

--- script.js
+++ script.js
@@ -2,19 +2,43 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-$(function () {});
+$(function () {
+  $(".saveBtn").on("click", function (event) {
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
+  });
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-var saveBtn = $(".saveBtn");
-saveBtn.on("click", function (event) {
-  var timeBlock = $(this).closest(".time-block");
+  // Function to add presnt, future, past class to each div
+  function changeColor() {
+    var currentTime = dayjs().hour();
+    $(".time-block").each(function () {
+      var blockTime = parseInt($(this).attr("id").split("-")[1]);
+      if (blockTime < currentTime) {
+        $(this).addClass("past");
+      } else if (blockTime === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  changeColor();
 
-  // Code to commit Event to Local Storage
-  var textArea = $(this).find(".description");
-  const inputtedValue = textArea.val();
-  localStorage.setItem("Event", inputtedValue);
-  console.log(inputtedValue);
-
-  const storedTextAreaVal = localStorage.getItem("Event");
+  // Adding current date to top of page
+  $("#currentDay").text(dayjs().format("YYYY-MM-DD"));
 });
 
 // TODO: Add a listener for click events on the save button. This code should
